97e625971c9c5e8eaabb993bebc9fc3eacfcbd3c
Rinkesh Mehar <rinkesh.mehar@cropdata.in>
2021-10-08 22:37:21 +0530

    init commit

---
+++ src/main/java/com/example/kafkaproducer/controller/KafkaController.java
@@ -0,0 +1,24 @@
+package com.example.kafkaproducer.controller;
+
+import com.example.kafkaproducer.model.KafkaModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    private KafkaTemplate<String, KafkaModel> kafkaTemplate;
+    private static final String TOPIC = "ujwal";
+
+    @GetMapping("/publish/{id}/{name}/{email}")
+    public String post(@PathVariable("id") final int id, @PathVariable("name") String name, @PathVariable("email") String email){
+        kafkaTemplate.send(TOPIC, new KafkaModel(id,name, email));
+
+        return "Published successfully";
+    }
+}
