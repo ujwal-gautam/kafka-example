8134a45fcb9c951106e383e96fecf1ed3692055b
Rinkesh Mehar <rinkesh.mehar@cropdata.in>
2021-10-09 15:09:56 +0530

    update json import package

--- src/main/java/com/example/kafkaproducer/config/SpringConfig.java
+++ src/main/java/com/example/kafkaproducer/config/SpringConfig.java
@@ -1,8 +1,7 @@
 package com.example.kafkaproducer.config;
 
-
 import com.example.kafkaproducer.model.KafkaModel;
-import com.fasterxml.jackson.databind.JsonSerializable;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -22,9 +21,9 @@ public class SpringConfig {
     public ProducerFactory<String, KafkaModel> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.51:29092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.51:39092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializable.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(config);
     }
